46a10b1aef785583273dfc919840902b7a676c38
Timothy Fraser, PhD <timothy.fraser.1@gmail.com>
2025-10-18 17:40:06 +0000

    x

--- demos/shinyapp/workflow.R
+++ demos/shinyapp/workflow.R
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-
-
-# test input data
-
-input = list(n = 30, type = "chihuahua")
-output = list(mydogs = NULL)
-
-
-=======
 # workflow.R
 
 # An optional script that you use to linearly test your functions before you put them into a ShinyApp dashboard in app.R
@@ -22,19 +12,13 @@ input = list(n = 30, type = "chihuahua")
 output = list(mydogs = NULL)
 
 # your function
->>>>>>> 5eb5c25bfe39982f47746d6ad9b94d511ffc1e52
 count_my_dogs = function(n, type){
   paste0(n, " ", type, "s")
 
 }
-<<<<<<< HEAD
 
-output$mydogs = count_my_dogs(n = input$n, type = input$type)
-
-=======
 # perform the operation
 output$mydogs = count_my_dogs(n = input$n, type = input$type)
 
 # Check your output object
->>>>>>> 5eb5c25bfe39982f47746d6ad9b94d511ffc1e52
 output
